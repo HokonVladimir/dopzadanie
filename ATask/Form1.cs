4bf88dd09f03d446beac3ff939f14dc0ab92e76f
vova.khokon.01@mail.ru <vova.khokon.01@mail.ru>
2020-04-07 12:37:51 +0300

    dopzadanie

---
+++ ATask/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ATask
+{
+    public partial class Form1 : Form
+    {
+        private string[] readText;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            button1.Click += Button1_Click;
+
+            
+            button2.Click += Button2_Click;
+
+            
+            button3.Click += Button3_Click;
+        }
+
+        private void Button3_Click(object sender, EventArgs e) {
+            try {
+                int shortLine = readText[0].Length;
+                int longLine = readText[0].Length;
+
+                
+                for (int z = 0; z < readText.Length; ++z) {
+                    if (readText[z].Length < shortLine) {
+                        shortLine = readText[z].Length;
+                    } if (readText[z].Length > longLine) {
+                        longLine = readText[z].Length;
+                    }
+                }
+
+                
+                int amountAdded = 0;
+                string[] bufferText = new string[readText.Length];
+                for (int z = 0; z < readText.Length; ++z) {
+                    if (readText[z].Length != shortLine &&
+                        readText[z].Length != longLine)
+                        bufferText[amountAdded++] = readText[z];
+                }
+                
+                
+                bufferText = copy(bufferText, amountAdded);
+
+                
+                
+                for (int z = 0; z < bufferText.Length; ++z) {
+                    char[] buf = bufferText[z].ToCharArray();
+
+                    for (int x = 0; x < buf.Length; x++) {
+                        if (buf[x] == ' ') buf[x++] = ',';
+                        if (buf[x] == ',') buf[x] = ' ';
+                    }
+
+                    bufferText[z] = new string(buf);
+                }
+                
+                foreach (string x in bufferText) {
+                    textBox4.Text += (x + "\r\n");
+                }
+
+            } catch (IndexOutOfRangeException error) {
+                textBox2.Text = error.Message;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e) {
+            textBox3.Text = string.Join("\r\n", readText);
+        }
+
+        private void Button1_Click(object sender, EventArgs e) {
+            try {
+                readText = File.ReadAllLines(textBox1.Text);
+                textBox2.Text = "Успешно открыт!";
+            } catch (IOException error) {
+                textBox2.Text = error.Message;
+            }
+        }
+
+        
+        private string[] copy(string[] c, int amountAdded) {
+            string[] b = new string[amountAdded];
+            for (int z = 0; z < amountAdded; ++z) {
+                b[z] = c[z];
+            } return b;
+        }
+    }
+}
---
+++ ATask/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApplication9
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int l = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[l];
+            for (int i = 0; i < l; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = 0;
+            for (int i = 0; i < l; i++)
+            {
+                if (array[i] % k == 0) sum += array[i];
+            }
+            Console.WriteLine("Сумма равна " + firstTask(array, k));
+            Console.WriteLine("2. ");
+            array = new int[l];
+            for (int i = 0; i < l; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            if (array[0] > 0) Console.WriteLine("Положительные"); else Console.WriteLine("Отрицательные ");
+            Console.WriteLine("3 ");
+            double[] arr1 = new double[l];
+            for (int i = 0; i < l; i++)
+            {
+                arr1[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine(isArrayIncreasing(arr1));
+            Console.WriteLine("4.");
+            array = new int[l];
+            for (int i = 0; i < l; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            countEvenNumbers(array);
+        }
+        private static void countEvenNumbers(int[] arr)
+        {
+            int evenSum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0) evenSum += arr[i];
+            }
+            if (evenSum == 0) Console.WriteLine("Четных чисел нет ");
+            Console.WriteLine("Четная сумма равна " + evenSum);
+        }
+        private static bool isArrayIncreasing(double[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1]) return false;
+            }
+            return true;
+        }
+        private static int firstTask(int[] arr, int k)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % k == 0) sum += arr[i];
+
+            }
+            return sum;
+        }
+    }
+}
